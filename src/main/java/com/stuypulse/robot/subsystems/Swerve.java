c68f01c711ddc0541cd898a73cd568a1d0155ecc
Myles Pasetsky <mp3blue99@gmail.com>
2022-07-24 13:50:04 -0400

    add swerve module

--- src/main/java/com/stuypulse/robot/subsystems/Swerve.java
+++
@@ -1,7 +0,0 @@
-package com.stuypulse.robot.subsystems;
-
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-public class Swerve extends SubsystemBase {
-
-}
---
+++ src/main/java/com/stuypulse/robot/subsystems/SwerveModule.java
@@ -0,0 +1,71 @@
+package com.stuypulse.robot.subsystems;
+
+import com.stuypulse.robot.util.Motor;
+import com.stuypulse.stuylib.control.Controller;
+import com.stuypulse.stuylib.control.angle.AngleController;
+import com.stuypulse.stuylib.control.angle.feedback.AnglePIDController;
+import com.stuypulse.stuylib.control.feedback.PIDController;
+import com.stuypulse.stuylib.control.feedforward.Feedforward;
+import com.stuypulse.stuylib.math.Angle;
+import com.stuypulse.stuylib.network.SmartAngle;
+import com.stuypulse.stuylib.network.SmartNumber;
+import com.stuypulse.stuylib.streams.angles.filters.AMotionProfile;
+
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/**
+ * An example swerve module where drive motor is controlled by a velocity 
+ * controller and the turn motor is controlled by a profiled position controller
+ * which is fed angles.
+ * 
+ * The velocity controllers includes a feedforward and a pid controller.
+ * 
+ * The position controller contains an angle pid controller and a feedforward
+ * that will work on the derivative of the positional setpoints. There is also 
+ * a filter on the setpoint that will apply a motion profile to them.
+ */
+public class SwerveModule extends SubsystemBase {
+
+    private final Motor turnMotor;
+    private final SmartAngle targetAngle;
+    private final AngleController turnController;
+
+    private final Motor driveMotor;
+    private final SmartNumber targetVelocity;
+    private final Controller driveController;
+
+    public SwerveModule() {
+        turnMotor = new Motor();
+        targetAngle = new SmartAngle("Swerve Module/Target Angle", Angle.kZero).useDegrees();
+        turnController = new AnglePIDController(0.1, 0.0, 0.0)
+            .add(new Feedforward.Motor(0.1, 0.08, 0.0).angle())
+            .setSetpointFilter(new AMotionProfile(1.0, 10.0));
+
+        driveMotor = new Motor();
+        targetVelocity = new SmartNumber("Swerve Module/Target Velocity", 0.0);
+        driveController = new PIDController(0.1, 0.0, 0.0)
+            .add(new Feedforward.Motor(0.1, 0.1, 0.1).velocity());
+    }
+
+    public void setTargetState(SwerveModuleState state) {
+        state = SwerveModuleState.optimize(state, turnMotor.getAngle().getRotation2d());
+        
+        targetAngle.set(Angle.fromRotation2d(state.angle));
+        targetVelocity.set(state.speedMetersPerSecond);
+    }
+
+    @Override
+    public void periodic() {
+        double turnOutput = turnController.update(targetAngle.get(), turnMotor.getAngle());
+        turnMotor.set(turnOutput);
+
+        double driveOutput = driveController.update(targetVelocity.get(), driveMotor.getVelocity());
+        driveMotor.set(driveOutput);
+
+        SmartDashboard.putNumber("Swerve Module/Measured Velocity", driveController.getMeasurement());
+        SmartDashboard.putNumber("Swerve Module/Measured Angle", turnController.getMeasurement().toDegrees());
+    }
+
+}
